46c0501bb5c4d0b2803a5e8f2a31e95397790e9a
ygtreceee <643788048@qq.com>
2022-11-03 23:34:15 +0800

    md

--- code.cpp/test.cpp
+++ code.cpp/test.cpp
@@ -1,9 +1,181 @@
-#include <stdio.h>
-#include <windows.h>
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <iomanip>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+
 int main()
 {
-    printf("Hello World\n");
-    system("pause");
+    int t;
+    char lib[100];
+    cin >> t;
+    while (t--)
+    {
+        memset(lib, '\0', 100);
+        int k;
+        cin >> lib >> k;
+        int len = strlen(lib);
+        while(k--)
+        {
+            for(int i = 0; i < len; i++)
+            {
+                if(lib[i] > lib[i + 1])
+                {
+                    for(int j = i; j < len; j++)    lib[j] = lib[j + 1];
+                    break;
+                }
+            }
+        }
+        cout << lib << endl;
+    }
     return 0;
 }
 
+#if 0
+//电池的寿命
+bool comparision(int a, int b)
+{
+    return a < b;
+}
+int main()
+{
+    int n;
+    double sum = 0;
+    int time[1010];
+    while (scanf("%d", &n) != EOF)
+    {
+        sum = 0;
+        for (int i = 0; i < n; i++)
+            cin >> time[i];
+        sort(time, time + n, comparision);
+        for(int i = 0; i < n; i++)
+        {
+            sum += time[i];
+            if(i == n - 1 && sum <= 2 * time[n - 1])   sum -= time[n - 1];
+            else if(i == n - 1 && sum > 2 * time[n - 1])    sum /= 2;
+        }
+        cout << fixed << setprecision(1) << sum << endl;
+    }
+    return 0;
+}
+
+//种树
+struct str
+{
+    int b;
+    int e;
+    int cnt;
+};
+
+bool comparition(str a, str b)
+{
+    return a.e < b.e;
+}
+int main()
+{
+    int path, n, sum = 0;
+    cin >> path >> n;
+    str adv[5100];
+    for(int i = 0; i < n; i++)  cin >> adv[i].b >> adv[i].e >> adv[i].cnt;
+    sort(adv, adv + n, comparition);
+    int book[31000] = {0};
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = adv[i].e; j >= adv[i].b; j--)
+        {
+            if(book[j] == 1)    adv[i].cnt--;    
+        }
+        for(int j = adv[i].e; j >= adv[i].b; j--)
+        {
+            if(adv[i].cnt <= 0) break;
+            if(book[j] == 0)
+            {
+                adv[i].cnt--;
+                sum++;
+                book[j] = 1;
+            }
+        }
+    }
+    cout << sum;
+    return 0;
+}
+
+
+//圣诞老人的礼物
+struct str
+{
+    int value;
+    int  weigh;
+};
+
+bool myfunction(str i, str j)
+{
+    return (1.0 * i.value / i.weigh) >(1.0 * j.value / j.weigh);
+}
+int main()
+{
+    int m, n;
+    double sum = 0;
+    cin >> m >> n;
+    str gift[1000];
+    for(int i = 0; i < m; i++)  cin >> gift[i].value >> gift[i].weigh;
+    sort(gift, gift + m, myfunction);
+    for(int i = 0; i < m; i++)
+    {
+        if(n > gift[i].weigh)   
+        {
+            sum += gift[i].value;
+            n = n - gift[i].weigh;
+        }
+        else    
+        {
+            sum += gift[i].value / gift[i].weigh * n* 1.0;
+            break;
+        }
+    }
+    cout << fixed << setprecision(1) << sum << endl;
+    return 0;
+}
+
+
+//电影节
+struct Film
+{
+    int f;
+    int e;
+};
+bool operator<(const Film &t, const Film &c)
+{
+    if (t.e == c.e)
+        return t.f < c.f;
+    else
+        return t.e < c.e;
+}
+int main()
+{
+    struct Film film[1100];
+    int n;
+    while (scanf("%d", &n) && n != 0)
+    {
+        for (int i = 0; i < 1100; i++)
+            film[i].e = film[i].f = 0;
+        for (int i = 0; i < n; i++)
+            cin >> film[i].f >> film[i].e;
+        sort(film, film + n);
+        int total = 0;
+        int first = 0;
+        for (int i = 0; i < n; i++)
+            if (film[i].f >= first)
+            {
+                total++;
+                first = film[i].e;
+            }
+        cout << total << endl;
+    }
+    return 0;
+}
+#endif
